9b7b01ba05d30f507de00e7c809c60586be9e49c
magl88 <magl88net@gmail.com>
2023-06-19 12:14:14 +0000

    add config for server

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
-  url: env("PUBLIC_URL", "http://localhost:1338"),
+  url: env("PUBLIC_URL", "https://api.edm-box.com"),
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  proxy: env.bool('IS_PROXIED', true),
   app: {
     keys: env.array("APP_KEYS"),
   },
